bce7c01dc56b71b3aae8c20bca4b3c19215430f5
ioana01 <balasoiu.ioana@gmail.com>
2022-04-10 22:55:11 +0300

    Updated navbar design with tailwind

--- idp-pweb/src/components/navbar/navbar.js
+++ idp-pweb/src/components/navbar/navbar.js
@@ -24,41 +24,55 @@ export default function BootstrapNavbar() {
         }
     }
 
-    return(
-        <div className='navbar-div'>
-            <div className="row">
-                <div className="col-md-12" id='navbar-container'>
-                    <Router>
-                        <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-                            <Navbar.Brand href="#home">Help Ukraine</Navbar.Brand>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                                <Navbar.Collapse id="basic-navbar-nav">
-                                    <Nav className="ml-auto">
-                                        <ul class="nav navbar-nav navbar-right">
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="/">Home</a>
-                                            </li>
-                                        </ul>
-                                        {!auth.currentUser ?
-                                        <ul class="nav navbar-nav navbar-right">
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="/login">Login</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" id="last-button" href="/register">Register</a>
-                                            </li>
-                                        </ul> :
-                                        <ul class="nav navbar-nav navbar-right">
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="/login" onClick={handleLogout}>Logout</a>
-                                            </li>
-                                        </ul>}
-                                    </Nav>
-                                </Navbar.Collapse>
-                        </Navbar>
-                    </Router>
+    function toggleNavbar(collapseID){
+        document.getElementById(collapseID).classList.toggle("hidden");
+        document.getElementById(collapseID).classList.toggle("flex");
+    }
+
+    return (
+        <nav class="flex items-center justify-between flex-wrap p-6">
+            <div class="block lg:hidden">
+                <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 burger-icon"
+                onClick={() => toggleNavbar('example-collapse-navbar')}>
+                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
+            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto" id="example-collapse-navbar">
+                <div class="text-sm lg:flex-grow">
+                    <a href="/about" class="block mt-3 lg:inline-block lg:mt-0 text-teal-200 mr-4 nav-item">
+                        About
+                    </a>
+                    <a href="/offers" class="block mt-3 lg:inline-block lg:mt-0 text-teal-200 mr-4 nav-item">
+                        Offers
+                    </a>
+                    <a href="/myRequests" class="block mt-3 lg:inline-block lg:mt-0 text-teal-200 mr-4 nav-item">
+                        My requests
+                    </a>
+                    <a href="/info" class="block mt-3 lg:inline-block lg:mt-0 text-teal-200 mr-4 nav-item">
+                        Emergency info
+                    </a>
+                </div>
+                <div class="text-sm">
+                    {!auth.currentUser ?
+                    <>
+                        <a href="/login" class="block mt-3 lg:inline-block lg:mt-0 text-teal-200 mr-4 nav-item">
+                            Login
+                        </a>
+                        <a href="/register" class="block mt-3 lg:inline-block lg:mt-0 text-teal-200 mr-4 nav-item">
+                            Register
+                        </a>
+                    </> : 
+                    <>
+                        <a href="/account" class="block mt-3 lg:inline-block lg:mt-0 text-teal-200 mr-4 nav-item">
+                            Account
+                        </a>
+                        <a href="/login" class="block mt-3 lg:inline-block lg:mt-0 text-teal-200 mr-4 nav-item"
+                        onClick={handleLogout}>
+                            Logout
+                        </a>
+                    </>}
                 </div>
             </div>
-        </div>
+        </nav>
     )  
 }
