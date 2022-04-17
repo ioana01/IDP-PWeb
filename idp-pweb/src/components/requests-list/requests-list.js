013bb6a2e4cc3a528e5a3b6485ae0bf9a1790047
Stefan Popa <andrei.popa2199@gmail.com>
2022-04-18 01:02:05 +0300

    Implement requests page

---
+++ idp-pweb/src/components/requests-list/requests-list.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import './requests-list.css';
+import SideMenu from "../side-menu/side-menu";
+import SearchBar from "../search-bar/search-bar";
+import Card from "../card/card";
+
+export default function RequestsList(){
+
+    const addNewSavedItem = (cardId) => console.log(cardId);
+
+        return (
+            <div className="grid grid-cols-6 gap-0">
+                <SideMenu/>
+                <div className="card-list col-span-4">
+                    <SearchBar/>
+                    <div className="cards-container lg:columns-2 gap-8">
+                        <Card addNewSavedItem={addNewSavedItem}/>
+                        <Card addNewSavedItem={addNewSavedItem}/>
+                        <Card addNewSavedItem={addNewSavedItem}/>
+                        <Card addNewSavedItem={addNewSavedItem}/>
+                    </div>
+                </div>
+            </div>
+        )
+}
