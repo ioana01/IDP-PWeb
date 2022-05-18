a1fc7b0853b962627a39c3a46d18d749580bd872
Stefan Popa <andrei.popa2199@gmail.com>
2022-05-18 20:15:26 +0300

    Update worker.py

--- worker/worker.py
+++ worker/worker.py
@@ -21,59 +21,48 @@ firebase_config = {
 firebase = pyrebase.initialize_app(firebase_config)
 auth = firebase.auth()
 
-print(' [*] Connecting to server ...')
+print(' [*] Connecting to RabbitMQ...')
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
-
-channel = connection.channel()
-channel.queue_declare(queue='task_queue', durable=True)
-channel.queue_declare(queue='verify_email_queue', durable=True)
-channel.queue_declare(queue='reset_password_queue', durable=True)
-print(' [*] Waiting for messages.')
-
-
-def demo_callback(ch, method, properties, body):
-    print(" [x] Received %s" % body)
-    cmd = body.decode()
-
-    if cmd == 'hey':
-        print("hey there")
-    elif cmd == 'hello':
-        print("well hello there")
-    else:
-        print("sorry i did not understand ", body)
-
-    print(" [x] Done")
-
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-
-
-def verify_email_callback(ch, method, properties, body):
+for i in range(0, 3):
     try:
-        payload = json.loads(body.decode())
-        print(" [x] Verifying email for %s..." % payload['email'])
-        auth.send_email_verification(payload['idToken'])
-        print(" [x] Done")
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-    except Exception as e:
-        print(" [x] Email verification could not be performed!")
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+        def verify_email_callback(ch, method, properties, body):
+            try:
+                payload = json.loads(body.decode())
+                print(" [x] Verifying email for %s..." % payload['email'])
+                auth.send_email_verification(payload['idToken'])
+                print(" [x] Done")
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+            except Exception as e:
+                print(" [x] Email verification could not be performed!")
+                ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-def reset_password_callback(ch, method, properties, body):
-    try:
-        print(" [x] Resetting password for...")
-        payload = body.decode()
-        auth.send_password_reset_email(payload)
-        print(payload)
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-    except Exception as e:
-        print(" [x] Password reset could not be performed!")
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+        def reset_password_callback(ch, method, properties, body):
+            try:
+                print(" [x] Resetting password for...")
+                payload = body.decode()
+                auth.send_password_reset_email(payload)
+                print(payload)
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+            except Exception as e:
+                print(" [x] Password reset could not be performed!")
+                ch.basic_ack(delivery_tag=method.delivery_tag)
 
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+        channel = connection.channel()
+        channel.queue_declare(queue='task_queue', durable=True)
+        channel.queue_declare(queue='verify_email_queue', durable=True)
+        channel.queue_declare(queue='reset_password_queue', durable=True)
+        print(' [*] Connected to RabbitMQ successfully!')
+        print(' [*] Waiting for messages.')
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='task_queue', on_message_callback=demo_callback)
-channel.basic_consume(queue='verify_email_queue', on_message_callback=verify_email_callback)
-channel.basic_consume(queue='reset_password_queue', on_message_callback=reset_password_callback)
-channel.start_consuming()
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(queue='verify_email_queue', on_message_callback=verify_email_callback)
+        channel.basic_consume(queue='reset_password_queue', on_message_callback=reset_password_callback)
+        channel.start_consuming()
+    except:
+        print(' [*] Connection to RabbitMQ failed! Retrying one more time...')
+        time.sleep(5)
+        continue
+    break
+
