56360b734f561165f36b987edb0065b12fba8369
Stefan Popa <andrei.popa2199@gmail.com>
2022-05-19 22:41:51 +0300

    Update react

--- idp-pweb/src/components/about/about.js
+++ idp-pweb/src/components/about/about.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import './about.css';
 import aboutLogo from './about-logo.svg';
 
@@ -7,7 +7,7 @@ export default function About(props) {
         <>
             <div className="lg:columns-2 gap-0 top-info">
                 <div className="about-logo">
-                    <img src={aboutLogo}></img>
+                    <img src={aboutLogo} alt='about-logo'/>
                 </div>
                 <div>
                     <h2 className="about-title">Together for Ukraine</h2>
@@ -19,23 +19,23 @@ export default function About(props) {
                 </div>    
             </div>
             <div className="grid lg:grid-cols-3 md:grid-cols-3 gap-8 steps">
-                <div>
-                    <h3 className="step-title">Step 1</h3>
-                    <p>
-                        Choose the type of account you would like to use ('I offer help' / 'I need help') and register.
-                    </p>
+                <div className="text-justify">
+                    <h3 className="step-title text-xl mb-2">Step 1</h3>
+                    <span>
+                        <strong className="step-title">Choose the type of account</strong> you would like to use (<strong>I offer help</strong> or <strong>I need help</strong>) and register. By choosing to <strong>offer help</strong>, you will accept the responsability of helping the ones in need. By choosing to <strong>request help</strong>, you will be able to view all help offers posted by the ones that provide help.
+                    </span>
                 </div>
-                <div>
-                    <h3 className="step-title">Step 2</h3>
-                    <p>
-                        Post a request/ an offer of help and a few details about it.
-                    </p>
+                <div className="text-justify">
+                    <h3 className="step-title text-xl mb-2">Step 2</h3>
+                    <span>
+                    <strong className="step-title">Post a help request or a help offer</strong> (depending on your account). If you request help, provide all details about your condition so that people can help you best. If you offer help, make sure to have all the resources you wish to provide. 
+                    </span>
                 </div>
-                <div>
-                    <h3 className="step-title">Step 3</h3>
-                    <p>
-                        Contact the author of the post and stay in touch.
-                    </p>
+                <div className="text-justify">
+                    <h3 className="step-title text-xl mb-2">Step 3</h3>
+                    <span>
+                        <strong className="step-title">Contact the author of the post</strong> and stay in touch. We wish to be flexible on the platform and encourage as much help as possible. Our purpose as a platform is to function as a <span>virtual panel</span> where people can post to offer or request help. 
+                    </span>
                 </div>
             </div>
         </>
